c8f2b51c21e74588b351822950ab4218a214aa18
Ali Tavallaie <a.tavallaie@gmail.com>
2019-12-01 22:11:29 +0330

    Import Label works now

--- image_labeling_extention/ImageLabelingExtention/ImageLabeling/ImageLabeling.py
+++ image_labeling_extention/ImageLabelingExtention/ImageLabeling/ImageLabeling.py
@@ -1,6 +1,8 @@
 import os
+import random
 import unittest
 import vtk, qt, ctk, slicer
+import vtkSegmentationCore
 from slicer.ScriptedLoadableModule import *
 import logging
 
@@ -38,7 +40,15 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
 
-  def setup(self):
+  def setup(self):        
+    self.label= []
+    self.layer = []
+    self.home = os.path.expanduser('~')
+    self.labeltargetdir= os.path.join(str(self.home),"SlicerLabel")
+    self.defaultlabel = os.path.join(str(self.labeltargetdir),"defaultlabel.txt")
+    self.status = self.default_label_exist()
+    self.inputSegmentationNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSegmentationNode')
+
     ScriptedLoadableModuleWidget.setup(self)
 
     # Instantiate and connect widgets ...
@@ -53,61 +63,13 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
     # Layout within the dummy collapsible button
     parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
 
-    #
-    # input volume selector
-    #
-    # self.inputSelector = slicer.qMRMLNodeComboBox()
-    # self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
-    # self.inputSelector.selectNodeUponCreation = True
-    # self.inputSelector.addEnabled = False
-    # self.inputSelector.removeEnabled = False
-    # self.inputSelector.noneEnabled = False
-    # self.inputSelector.showHidden = False
-    # self.inputSelector.showChildNodeTypes = False
-    # self.inputSelector.setMRMLScene( slicer.mrmlScene )
-    # self.inputSelector.setToolTip( "Pick the input to the algorithm." )
-    # parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
-
-    #
-    # output volume selector
-    #
-    # self.outputSelector = slicer.qMRMLNodeComboBox()
-    # self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
-    # self.outputSelector.selectNodeUponCreation = True
-    # self.outputSelector.addEnabled = True
-    # self.outputSelector.removeEnabled = True
-    # self.outputSelector.noneEnabled = True
-    # self.outputSelector.showHidden = False
-    # self.outputSelector.showChildNodeTypes = False
-    # self.outputSelector.setMRMLScene( slicer.mrmlScene )
-    # self.outputSelector.setToolTip( "Pick the output to the algorithm." )
-    # parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
-
-    #
-    # threshold value
-    #
-    # self.imageThresholdSliderWidget = ctk.ctkSliderWidget()
-    # self.imageThresholdSliderWidget.singleStep = 0.1
-    # self.imageThresholdSliderWidget.minimum = -100
-    # self.imageThresholdSliderWidget.maximum = 100
-    # self.imageThresholdSliderWidget.value = 0.5
-    # self.imageThresholdSliderWidget.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
-    # parametersFormLayout.addRow("Image threshold", self.imageThresholdSliderWidget)
-
-    #
-    # check box to trigger taking screen shots for later use in tutorials
-    #
-    # self.enableScreenshotsFlagCheckBox = qt.QCheckBox()
-    # self.enableScreenshotsFlagCheckBox.checked = 0
-    # self.enableScreenshotsFlagCheckBox.setToolTip("If checked, take screen shots for tutorials. Use Save Data to write them to disk.")
-    # parametersFormLayout.addRow("Enable Screenshots", self.enableScreenshotsFlagCheckBox)
-
+ 
     #
     # Import Default Layer Button aka IDLButton
     #
     self.IDLButton = qt.QPushButton("Import Default Layer")
     self.IDLButton.toolTip = "import default layers."
-    self.IDLButton.enabled = False
+    self.IDLButton.enabled = self.status
     parametersFormLayout.addRow(self.IDLButton)
     #
     # Imort Label Button
@@ -115,6 +77,13 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
     self.importLabelButton = qt.QPushButton("Import Label")
     self.importLabelButton.toolTip = "import label from file."
     self.importLabelButton.enabled = True
+    parametersFormLayout.addRow(self.importLabelButton)
+    #
+    # Imort layer Button
+    #
+    self.importLayerButton = qt.QPushButton("Import layer")
+    self.importLayerButton.toolTip = "import layer from file."
+    self.importLayerButton.enabled = True
     parametersFormLayout.addRow(self.importLabelButton)
     #
     # Save Button
@@ -135,7 +104,7 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
     # connections
     
     # self.applyButton.connect('clicked(bool)', self.onApplyButton)
-    self.importLabelButton.connect('clicked(bool)', self.onImportLayerButton)
+    self.importLabelButton.connect('clicked(bool)', self.onImportLabelButton)
 
     # self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
     # self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
@@ -158,8 +127,15 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
   def onIDLButton(self):
     pass
 
-  def onImportLayerButton(self):
+  def onImportLabelButton(self):
+    # import label from file 
     name = self.file_open()
+    self.label = []
+    self.import_labeltext(name)
+    self.Label2layer()
+    self.add_layer()
+    self.save_layer()
+    self.IDLButton.enabled = True
     self.importLabelButton.toolTip = name
 
   def onSaveLayerButton(self):
@@ -167,7 +143,69 @@ class ImageLabelingWidget(ScriptedLoadableModuleWidget):
 
   def onExportLayerButton(self):
     pass
+  
 
+  # Semi-Logic
+  def import_labeltext(self,labelfile):
+        # import label from file 
+        labelfile = os.path.abspath(labelfile)
+        with open(labelfile,"r") as labelfile:
+            for label in labelfile.read().split(" "):
+                self.label.append(label) 
+        return self.label
+
+  def default_label_exist(self):
+      if os.path.exists(self.defaultlabel):
+        self.status = True
+      else:
+        self.status = False
+      return self.status
+
+  def layer_initiator(self):
+      if not os.path.exists(self.labeltargetdir):
+          os.makedirs(self.labeltargetdir)
+      if not os.path.exists(self.defaultlabel):
+          labels=open(self.defaultlabel,"w")
+          labels.close()
+      self.default_label_exist()
+      return self.status 
+
+  def load_default_label(self):
+        #check for default layer
+        if self.status:
+            if os.path.getsize(str(self.defaultlabel)):
+                with open(self.defaultlabel,"r") as labels:
+                    layer = labels.read()
+                    self.layer = layer
+                    print(self.layer)
+        else:
+            self.layer_initiator()
+        return self.status
+  def Label2layer(self):
+        # add color to label for making layers
+        layers=[]
+        for label in self.label:
+            layers.append([label,[random.uniform(0.0,1.0),random.uniform(0.0,1.0),random.uniform(0.0,1.0)]])
+        self.layer = layers
+        return layers
+  def add_layer(self):
+    # add layer to slicer
+    for layer in self.layer:
+      print layer[0]
+      print layer[1]
+      Segment = vtkSegmentationCore.vtkSegment()
+      Segment.SetName(layer[0])
+      Segment.SetColor(layer[1])
+      self.inputSegmentationNode.GetSegmentation().AddSegment(Segment) 
+  def save_layer(self):
+    # save defualt layer
+    if self.status:
+        with open(self.defaultlabel,"w+") as labelfile:
+            labelfile.write(str(self.layer))
+            labelfile.close()
+    else:
+        self.layer_initiator()
+    return self.status
   # def onApplyButton(self):
   #   logic = ImageLabelingLogic()
   #   enableScreenshotsFlag = self.enableScreenshotsFlagCheckBox.checked
